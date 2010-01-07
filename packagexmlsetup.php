e9d7688f656e6fbd2958bdaf3f9b6fc7bade3fc3
Brett Bieber <brett.bieber@gmail.com>
2010-01-07 15:43:13 -0600

    Fix pearinstaller version requirement

--- packagexmlsetup.php
+++ packagexmlsetup.php
@@ -4,3 +4,5 @@
  */
 $package->dependencies['required']->php = '5.2.0';
 $compatible->dependencies['required']->php = '5.2.0';
+$package->dependencies['required']->pearinstaller = '1.4.0';
+$compatible->dependencies['required']->pearinstaller = '1.4.0';
