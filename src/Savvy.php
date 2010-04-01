a07e3a747f146755c1114b69d94f8ed2760f72d6
Brett Bieber <brett.bieber@gmail.com>
2010-04-01 09:29:44 -0500

    Add renderNull method

--- src/Savvy.php
+++ src/Savvy.php
@@ -674,6 +674,21 @@ class Savvy
         return $this->fetch($object, $template);
     }
     
+    /**
+     * Used to render null through an optional template
+     * 
+     * @param null   $null     The null var
+     * @param string $template Template to render null through
+     * 
+     * @return string Rendered output
+     */
+    protected function renderNULL($null, $template = null)
+    {
+        if ($template) {
+            return $this->fetch(null, $template);
+        }
+    }
+    
     protected function fetch($mixed, $template = null)
     {
         if ($template) {
