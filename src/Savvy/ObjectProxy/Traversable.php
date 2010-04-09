30208b667cb98c8e729347eab3cf4a1debaed805
Brett Bieber <brett.bieber@gmail.com>
2010-04-09 10:02:25 -0500

    Switch from IteratorAggregate to Iterator to ensure vars are filtered correctly.

--- src/Savvy/ObjectProxy/Traversable.php
+++ src/Savvy/ObjectProxy/Traversable.php
@@ -1,13 +1,29 @@
 <?php
-class Savvy_ObjectProxy_Traversable extends Savvy_ObjectProxy implements IteratorAggregate
+class Savvy_ObjectProxy_Traversable extends Savvy_ObjectProxy implements Iterator
 {
-    function getIterator()
+
+    function next()
     {
-        return $this->object;
+        $this->object->next();
+    }
+
+    function key()
+    {
+        return $this->object->key();
+    }
+
+    function valid()
+    {
+        return $this->object->valid();
+    }
+
+    function rewind()
+    {
+        $this->object->rewind();
     }
 
     function current()
     {
-        return self::filterVar(parent::current());
+        return $this->filterVar($this->object->current());
     }
 }
