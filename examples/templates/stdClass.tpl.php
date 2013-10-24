b4d63c95319e727ce1c2ae443692609b170fc975
Brett Bieber <brett.bieber@gmail.com>
2013-10-24 13:43:21 -0500

    Minor HTML validation fix

--- examples/templates/stdClass.tpl.php
+++ examples/templates/stdClass.tpl.php
@@ -2,5 +2,5 @@
 <p>This represents the default mapping of class name to template.</p>
 <p>The class of this context is: <?php echo get_class($context); ?></p>
 <p>The $context object contains the stdClass object with access to all the member
-variables and functions, such as $context->var1</p>
+variables and functions, such as $context-&gt;var1</p>
 <?php echo $context->var1; ?>
