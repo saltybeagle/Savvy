saltybeagle/Savvy

commit 9efe7a6944ad531630faf45fc16d82299363bc58

Don't convert underscores in namespaces to directory separators
This follows psr-0 and will better reflect the directory structure of classes

src/Savvy/ClassToTemplateMapper.php
@@ -66,35 +66,40 @@ class Savvy_ClassToTemplateMapper implements Savvy_MapperInterface
     /**
      * Maps class names to template filenames.
      *
-     * Underscores and namespace separators in class names are replaced with
+     * This maps according to the PSR-0 standard -- 
+     * https://github.com/php-fig/fig-standards/blob/master/accepted/PSR-0.md
+     * 
+     * Underscores in the class name and namespace separators are replaced with
      * directory separators.
      *
      * Examples:
      * Class           => Class.tpl.php
      * Other_Class     => Other/Class.tpl.php
      * namespace\Class => namespace/Class.tpl.php
+     * name_space\Other_Class => name_space/Other/Class.tpl.php
      *
      * @param string $class Class name to map to a template
      *
      * @return string Template file name
      */
-    public function map($class)
+    function map($class)
     {
         if (isset(self::$output_template[$class])) {
             $class = self::$output_template[$class];
         }
 
-        $class = str_replace(array(self::$classname_replacement,
-                                   self::$directory_separator,
-                                   '\\'),
-                             array('',
-                                   DIRECTORY_SEPARATOR,
-                                   DIRECTORY_SEPARATOR),
-                             $class);
+        $className = ltrim($class, '\\');
+        $fileName  = '';
+        $namespace = '';
+        if ($lastNsPos = strrpos($className, '\\')) {
+            $namespace = substr($className, 0, $lastNsPos);
+            $className = substr($className, $lastNsPos + 1);
+            $fileName  = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
+        }
 
-        $templatefile = $class . self::$template_extension;
+        $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . self::$template_extension;
 
-        return $templatefile;
+        return $fileName;
     }
 
 }

commit bc8e4c8b852de663c3d26a4e5c2e61f2ef234fbb

Fix regressions for ClassToTemplateMapper replacements
Regressions can be found in https://github.com/saltybeagle/Savvy/commit/9efe7a6944ad531630faf45fc16d82299363bc58
Which are sadly, my fault.

src/Savvy/ClassToTemplateMapper.php
@@ -88,16 +88,17 @@ class Savvy_ClassToTemplateMapper implements Savvy_MapperInterface
             $class = self::$output_template[$class];
         }
 
+        $class = str_replace(self::$classname_replacement, '', $class);
+
         $className = ltrim($class, '\\');
         $fileName  = '';
-        $namespace = '';
         if ($lastNsPos = strrpos($className, '\\')) {
             $namespace = substr($className, 0, $lastNsPos);
             $className = substr($className, $lastNsPos + 1);
             $fileName  = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
         }
 
-        $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . self::$template_extension;
+        $fileName .= str_replace(self::$directory_separator, DIRECTORY_SEPARATOR, $className) . self::$template_extension;
 
         return $fileName;
     }