daaf7cd5be00f0f6271424d3ded4c920e92a90e6
Kevin Abel <kevin.abel.0@gmail.com>
2015-08-24 00:58:05 -0500

    Fix logic errors relating to Compiler classes

--- src/Savvy/BasicFastCompiler.php
+++ src/Savvy/BasicFastCompiler.php
@@ -13,12 +13,12 @@ class Savvy_BasicFastCompiler implements Savvy_FastCompilerInterface
      *
      * @param string $compiledtemplatedir Where to store compiled templates
      *
-     * @throws UnexpectedValueException
+     * @throws Savvy_UnexpectedValueException
      */
     public function __construct($compiledtemplatedir)
     {
         $this->compiledtemplatedir = realpath($compiledtemplatedir);
-        if (!$this->compiledtemplatedir && !is_writable($this->compiledtemplatedir)) {
+        if (!$this->compiledtemplatedir || !is_writable($this->compiledtemplatedir)) {
             throw new Savvy_UnexpectedValueException('Unable to compile templates into ' .
                                                $compiledtemplatedir . ', directory does not exist ' .
                                                'or is unwritable');
@@ -30,22 +30,20 @@ class Savvy_BasicFastCompiler implements Savvy_FastCompilerInterface
      * Compile a template.
      *
      * @param string $name  Template to compile
-     * @param Main   $savvy Savvy main object
+     * @param Savvy   $savvy Savvy main object
      *
      * @return string Name of the compiled template file.
      */
     public function compile($name, $savvy)
     {
         $cname = $this->compiledtemplatedir . md5($name);
-        if (file_exists($cname)) {
-            if (filemtime($name) == filemtime($cname)) {
-                return $cname;
-            }
+        if (file_exists($cname) && filemtime($name) <= filemtime($cname)) {
+            return $cname;
         }
         $a = file_get_contents($name);
         $a = "<?php return '" . str_replace(array('<?php echo', '?>'), array('\' . ', ' . \''), $a) . "';";
         file_put_contents($cname, $a);
-        touch($cname, filemtime($name));
+        touch($cname);
 
         return $cname;
     }
